b789a8858dc2dbf98d25745597ba508f2fb7759a
kirill_khnychkin <khnychkin_kirill@tutanota.com>
2021-08-23 01:27:19 +0300

    finished 03 - TypeScript compiler

---
+++ 03 - TS Compiler/src/app.ts
@@ -0,0 +1,10 @@
+const a: string = 'lol';
+console.log(a);
+console.log('asd');
+
+const button = document.querySelector('#button')!;
+
+button.addEventListener('click', () => {
+  console.log('click');
+});
+
